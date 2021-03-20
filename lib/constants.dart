6b628be961e8dcd9e0b4856610c152e80c4c0e88
Andre Mashraghi <andre.mashraghi@gmail.com>
2021-03-20 11:02:31 -0400

    Untrack files in .gitignore

---
+++ lib/constants.dart
@@ -0,0 +1,16 @@
+const double defaultPadding = 10;
+const double borderRadius = 12;
+const double containerBorderRadius = 15;
+const double maxContainerWidth = 600;
+const double maxButtonWidth = 600;
+const double minButtonWidth = 88;
+const double minButtonHeight = 50;
+const double backButtonSize = 30;
+
+// Settings page
+const double spaceBetweenSettingsItems = 25;
+
+/// Paths
+// Images
+final String imagesPath = 'assets/images/';
+final String logoImage = 'novaOneLogo.png';
